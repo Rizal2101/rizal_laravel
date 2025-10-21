b7d75726fae7c16a1fcb5788927c8a161a7047db
Rizal Amri Ritonga <rizalamri2707@gmail.com>
2025-10-21 21:54:38 +0700

    Pengenalan Route

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,20 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return 'Halo Laravel';
+});
+
+Route::get('/about', function () {
+    // return view('welcome');
+    return 'Ini adalah halaman <b>about</b>';
+});
+
+Route::get('/mahasiswa', function () {
+    return 'Ini adalah halaman <b>mahasiswa </b>';
+});
+
+Route::get('/profile', function () {
+    $nama = 'Rizal Amri Khoirul Hakim';
+    return 'Ini adalah halaman <b>profile</b>, nama saya adalah ' . $nama;
 });
