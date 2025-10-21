6885e997b4fa5eee9f3fee8a232b222eaee9a525
Rizal Amri Ritonga <rizalamri2707@gmail.com>
2025-10-21 22:03:31 +0700

    mengenal view laravel

--- routes/web.php
+++ routes/web.php
@@ -4,19 +4,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     // return view('welcome');
-    return 'Halo Laravel';
+     return view('index');
 });
 
 Route::get('/about', function () {
     // return view('welcome');
-    return 'Ini adalah halaman <b>about</b>';
+    return view('about');
 });
 
 Route::get('/mahasiswa', function () {
-    return 'Ini adalah halaman <b>mahasiswa </b>';
+    return view('mahasiswa');
 });
 
 Route::get('/profile', function () {
     $nama = 'Rizal Amri Khoirul Hakim';
-    return 'Ini adalah halaman <b>profile</b>, nama saya adalah ' . $nama;
+    return view('profile')->with('nama', $nama);
 });
